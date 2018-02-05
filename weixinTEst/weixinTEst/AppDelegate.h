7b7a881b7d64315abafd79bfab089ab3c06da85d
yzz <13027846338@163.com>
2018-02-05 16:22:25 +0800

    初始化 weixn

---
+++ weixinTEst/weixinTEst/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weixinTEst
+//
+//  Created by Huntersun on 2018/2/5.
+//  Copyright © 2018年 Huntersun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weixinTEst/weixinTEst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weixinTEst
+//
+//  Created by Huntersun on 2018/2/5.
+//  Copyright © 2018年 Huntersun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
